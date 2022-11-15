f535b4cb3b980aca50ed4162a4ccafa65378e398
phuoc_hacker_101 <97019672+hophuoc1403@users.noreply.github.com>
2022-11-15 11:53:13 +0700

    Update connect.php

--- admin/connect/connect.php
+++ admin/connect/connect.php
@@ -4,8 +4,25 @@
 
 $conn = mysqli_connect("localhost","phuocdepzai","123456","shopping");
 
-//if($conn->connect_error){
-//    echo $conn->errno;
-//};
+if($conn->connect_error){
+   echo $conn->errno;
+};
+
+function getData($table,$id=0){
+	global $conn;
+	$queryId = $id ? "where id = $id " : "";
+	$query =  "select * from $table $queryId";
+
+	return mysqli_query($conn,$query);
+}
+
+function addData($table,$data){
+
+
+	$keyData = "";
+	foreach ($table as $key => $value) {
+		$keyData .= $key . ",";
+	}
+}
 
 ?>
